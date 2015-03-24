0be85c0d11da81f1a6ac27dae4ad101d36aee83e
Vincent Berthiaume <vberthiaume@gmail.com>
2015-03-24 14:25:15 -0400

    Initial Commit

---
+++ PunchClock/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PunchClock
+//
+//  Created by Nicolai on 2015-03-24.
+//  Copyright (c) 2015 BMP4. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
