54cfe2e92b73e8b52c5a6e9c0828fb0a26bdb7d9
Vincent Berthiaume <vberthiaume@gmail.com>
2015-03-24 20:34:01 -0400

    now using a class

--- PunchClock/main.cpp
+++ PunchClock/main.cpp
@@ -2,7 +2,7 @@
 //  main.cpp
 //  PunchClock
 //
-//  Created by Nicolai on 2015-03-24.
+//  Created by Vincent Berthiaume on 2015-03-24.
 //  Copyright (c) 2015 BMP4. All rights reserved.
 //
 
@@ -18,20 +18,19 @@
 using namespace std;
 
 enum Projects {
-    GRIS = 0,
-    sBMP4,
-    SonicBirth,
+    Project0 = 0,
+    Project1,
+    Project2,
     TotalProjectCount
 };
 
 static string ProjectNames[] = {
-    "GRIS",
-    "sBMP4",
-    "SonicBirth"
+    "Project0",
+    "Project1",
+    "Project2"
 };
 
-
-string time2string(chrono::time_point<chrono::system_clock> p_time)
+static string time2string(chrono::time_point<chrono::system_clock> p_time)
 {
     auto in_time_t = chrono::system_clock::to_time_t(p_time);
     
@@ -40,7 +39,7 @@ string time2string(chrono::time_point<chrono::system_clock> p_time)
     return ss.str();
 }
 
-string time2date(chrono::time_point<chrono::system_clock> p_time)
+static string time2date(chrono::time_point<chrono::system_clock> p_time)
 {
     auto in_time_t = chrono::system_clock::to_time_t(p_time);
     
@@ -49,107 +48,141 @@ string time2date(chrono::time_point<chrono::system_clock> p_time)
     return ss.str();
 }
 
-void calculateTime(long elapsedTime, long& workSeconds, long& workMinutes, long& workHours){
-    workSeconds = elapsedTime;
-    workHours  = floor(workSeconds  / 3600);
-    workSeconds -= workHours * 3600;
-    workMinutes = floor(workSeconds / 60);
-    workSeconds -= workMinutes * 60;
-}
-
-void calculateTime(chrono::duration<double> elapsedTime, long& workSeconds, long& workMinutes, long& workHours){
-    calculateTime(chrono::duration_cast<chrono::seconds>(elapsedTime).count(), workSeconds, workMinutes, workHours);
-}
-
-int main(int argc, const char * argv[]) {
+class Bmp4PunchClock {
     
-    bool bIsPunchedIn = false;
+    bool bIsPunchedIn;
     chrono::time_point<chrono::system_clock> now, punchInTime, punchOutTime;
     chrono::duration<double> elapsedTime;
-    int iSelectedProject = -1;;
+    
     string strSelectedOption;
     vector<long> vAllTimes;
     long hours, minutes, seconds;
-
-    ofstream fileOutputStream = ofstream("PunchClockHours.txt", ios::app);
-    if(!fileOutputStream) cerr << "error";
     
+    ofstream fileOutputStream;
     
-    // ------------------ PROJECT SELECTION ------------------
-    cout << "************ BMP4 PUNCH CLOCK ************\n\n\n";
-    cout << "Pick your project for today!\n";
-    for (int iCurProject = 0; iCurProject < TotalProjectCount; ++iCurProject){
-        cout << "[" << iCurProject << "] " <<  ProjectNames[iCurProject] << "\n";
+    void punchIn(){
+        bIsPunchedIn = false;
+        
+        punchOutTime = chrono::system_clock::now();
+        cout << "Punched out at " << time2string(punchOutTime) << "\n";
+        elapsedTime = punchOutTime - punchInTime;
+        
+        //elapsedTime = chrono::seconds(3661);
+        
+        vAllTimes.push_back(chrono::duration_cast<chrono::seconds>(elapsedTime).count());
+        
+        calculateTime(elapsedTime, seconds, minutes, hours );
+        
+        cout << "time elapsed since last punch-in: "  << hours << ":" << minutes << ":" << seconds << "\n";
+        
+        fileOutputStream << hours << ":" << minutes << ":" << seconds << "\n";
+    }
+    
+    void punchOut(){
+        bIsPunchedIn = true;
+        punchInTime = chrono::system_clock::now();
+        cout << "Punched in at " << time2string(punchInTime) << "\n";
     }
 
-    do {
-        cout << ">";
-        cin >> iSelectedProject;
+public:
+    
+    int iSelectedProject;
+    
+    Bmp4PunchClock():
+    bIsPunchedIn(false),
+    iSelectedProject(-1) {
+        fileOutputStream = ofstream("PunchClockHours.txt", ios::app);
+        if(!fileOutputStream) cerr << "error";
+    }
+    
+    ~Bmp4PunchClock(){
         
-    } while (iSelectedProject < GRIS || iSelectedProject >= TotalProjectCount);
+        if (bIsPunchedIn){
+            punchOut();
+        }
+        
+        long totalTime = 0;
+        for (auto it = vAllTimes.begin(); it != vAllTimes.end(); ++it){
+            totalTime += *it;
+        }
+        
+        calculateTime(totalTime, seconds, minutes, hours);
+        
+        cout << "You worked a total of : "  << hours << ":" << minutes << ":" << seconds << " on project " << ProjectNames[iSelectedProject] << "\n";
+        
+        now = chrono::system_clock::now();
+        
+        fileOutputStream << "TOTAL "  << hours << ":" << minutes << ":" << seconds << "\n";
+        
+        fileOutputStream.close();
+    }
     
-    cout << "OK, good luck with: " << ProjectNames[iSelectedProject] << "!\n";
-
-    now = chrono::system_clock::now();
-    fileOutputStream << "--------------------------------\n";
-    fileOutputStream << ProjectNames[iSelectedProject] << " - " << time2date(now) << "\n";
-    
-    
-    // ------------------ PUNCH IN AND OUT ------------------
-    cout << "\ntype <p> to punch in and out, or <x> to exit.\n\n";
-    do {
-        strSelectedOption = "";
+    void projectSelection(){
+        cout << "************ BMP4 PUNCH CLOCK ************\n\n\n";
+        cout << "Pick the project you want to work on!\n";
+        for (int iCurProject = 0; iCurProject < TotalProjectCount; ++iCurProject){
+            cout << "[" << iCurProject << "] " <<  ProjectNames[iCurProject] << "\n";
+        }
+        
         do {
             cout << ">";
-            cin >> strSelectedOption;
+            cin >> iSelectedProject;
             
-        } while (strSelectedOption != "p" && strSelectedOption != "x");
+        } while (iSelectedProject < 0 || iSelectedProject >= TotalProjectCount);
         
-        //punching out
-        if (bIsPunchedIn){
-            bIsPunchedIn = false;
-            
-            punchOutTime = chrono::system_clock::now();
-            cout << "Punched out at " << time2string(punchOutTime) << "\n";
-            elapsedTime = punchOutTime - punchInTime;
-            
-            //elapsedTime = chrono::seconds(3661);
-            
-            vAllTimes.push_back(chrono::duration_cast<chrono::seconds>(elapsedTime).count());
+        cout << "OK, good luck with: " << ProjectNames[iSelectedProject] << "! ";
+        
+        now = chrono::system_clock::now();
+        fileOutputStream << "--------------------------------\n";
+        fileOutputStream << ProjectNames[iSelectedProject] << " - " << time2date(now) << "\n";
 
-            calculateTime(elapsedTime, seconds, minutes, hours );
-
-            cout << "time elapsed since last punch-in: "  << hours << ":" << minutes << ":" << seconds << "\n";
-            
-            fileOutputStream << hours << ":" << minutes << ":" << seconds << "\n";
-            
-        }
-        
-        //punching in
-        else {
-            bIsPunchedIn = true;
-            punchInTime = chrono::system_clock::now();
-            cout << "Punched in at " << time2string(punchInTime) << "\n";
-        }
-        
-    } while (strSelectedOption != "x");
-   
-    
-    // ------------------ EXITING ------------------
-    long totalTime = 0;
-    for (auto it = vAllTimes.begin(); it != vAllTimes.end(); ++it){
-        totalTime += *it;
     }
     
-    calculateTime(totalTime, seconds, minutes, hours);
+    void waitForPunches(){
+        // ------------------ PUNCH IN AND OUT ------------------
+        cout << "Type <p> to punch in and out, or <x> to exit.\n\n";
+        do {
+            strSelectedOption = "";
+            do {
+                cout << ">";
+                cin >> strSelectedOption;
+                
+            } while (strSelectedOption != "p" && strSelectedOption != "x");
+            
+            //punching out
+            if (bIsPunchedIn){
+                punchIn();
+            }
+            
+            //punching in
+            else {
+                punchOut();
+            }
+            
+        } while (strSelectedOption != "x");
+        
+    }
     
-    cout << "this chunk of work lasted: "  << hours << ":" << minutes << ":" << seconds << "\n";
-
-    now = chrono::system_clock::now();
+    void calculateTime(long elapsedTime, long& workSeconds, long& workMinutes, long& workHours){
+        workSeconds = elapsedTime;
+        workHours  = floor(workSeconds  / 3600);
+        workSeconds -= workHours * 3600;
+        workMinutes = floor(workSeconds / 60);
+        workSeconds -= workMinutes * 60;
+    }
     
-    fileOutputStream << "TOTAL "  << hours << ":" << minutes << ":" << seconds << "\n";
+    void calculateTime(chrono::duration<double> elapsedTime, long& workSeconds, long& workMinutes, long& workHours){
+        calculateTime(chrono::duration_cast<chrono::seconds>(elapsedTime).count(), workSeconds, workMinutes, workHours);
+    }
+};
 
-    fileOutputStream.close();
+int main(int argc, const char * argv[]) {
+    
+    Bmp4PunchClock punchClock;
+    
+    punchClock.projectSelection();
+    
+    punchClock.waitForPunches();
     
     return 0;
 }
